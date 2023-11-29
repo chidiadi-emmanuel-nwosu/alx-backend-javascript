8fd9c6a03fcbe1aecb3cb483040e2a18476c4a1b
chidiadi-emmanuel-nwosu <chidiadi.emmanuel.nwosu@gmail.com>
2023-11-29 21:59:02 +0100

    refactored task 6

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -3,8 +3,10 @@ import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
   return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
-    .then((results) => results.map((result) => ({
-      status: `${result.status}`,
-      value: result.status === 'fulfilled' ? `${result.value}` : `${result.reason}`,
-    })));
+    .then((results) => {
+      const array = results.map((result) => (
+        { status: result.status, value: result.value || result.reason }
+      ));
+      return array;
+    });
 }
