fd38a67a0df96fca5c1c299c6175b096549785a4
chidiadi-emmanuel-nwosu <chidiadi.emmanuel.nwosu@gmail.com>
2023-12-04 19:26:17 +0100

    debugging task 8 - 3

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,20 +1,12 @@
-// export default function cleanSet(set, startString) {
-//   if (!set || !(set instanceof Set) || !startString || !startString.length) return '';
-
-//   const stringSet = [...set].filter((element) => typeof element === 'string');
-
-//   const cleanedString = stringSet
-//     .filter((element) => element.startsWith(startString))
-//     .map((element) => element.slice(startString.length))
-//     .join('-');
-
-//   return cleanedString.length > 0 ? cleanedString : '';
-// }
-//
 export default function cleanSet(set, startString) {
   if (!set || !(set instanceof Set) || !startString || !startString.length) return '';
-  return [...set]
+
+  const stringSet = [...set].filter((element) => typeof element === 'string');
+
+  const cleanedString = stringSet
     .filter((element) => element.startsWith(startString))
     .map((element) => element.slice(startString.length))
     .join('-');
+
+  return cleanedString;
 }
