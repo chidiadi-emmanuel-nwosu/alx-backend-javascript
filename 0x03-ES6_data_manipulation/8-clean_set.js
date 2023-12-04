1ee604f4d0c3963b4dd273dc87734ec4fe18e27d
chidiadi-emmanuel-nwosu <chidiadi.emmanuel.nwosu@gmail.com>
2023-12-04 19:47:19 +0100

    refactored task 8

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,10 +1,8 @@
 export default function cleanSet(set, startString) {
   if (!startString || !startString.length) return '';
 
-  const stringSet = [...set].filter((element) => typeof element === 'string');
-
-  const cleanedString = stringSet
-    .filter((element) => element.startsWith(startString))
+  const cleanedString = [...set]
+    .filter((element) => typeof element === 'string' && element.startsWith(startString))
     .map((element) => element.slice(startString.length))
     .join('-');
 
