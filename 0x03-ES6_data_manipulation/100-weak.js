01c1dcb2e2c6e47a6b7ea8d2168acb3d6876e1b4
chidiadi-emmanuel-nwosu <chidiadi.emmanuel.nwosu@gmail.com>
2023-12-05 10:06:51 +0100

    debugging advanced task

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -9,7 +9,7 @@ export function queryAPI(endpoint) {
 
   if (isValidEndpoint) {
     const queryCount = weakMap.get(endpoint) || 0;
-    if (queryCount >= 5) throw new Error('Endpoint load is high');
+    if (queryCount + 1 >= 5) throw new Error('Endpoint load is high');
     weakMap.set(endpoint, queryCount + 1);
   }
 }
