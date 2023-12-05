8b35386106cfb98cf47247412836c08a911aec1a
chidiadi-emmanuel-nwosu <chidiadi.emmanuel.nwosu@gmail.com>
2023-12-05 09:41:19 +0100

    tried fixing error for advanced task

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -1,11 +1,19 @@
 export const weakMap = new WeakMap();
 
 export function queryAPI(endpoint) {
-  if (!weakMap.get(endpoint)) {
-    weakMap.set(endpoint, 1);
-  } else if (weakMap.get(endpoint) >= 5) {
-    throw new Error('Endpoint load is high');
-  } else {
-    weakMap.set(endpoint, weakMap.get(endpoint) + 1);
+  const isValidEndpoint = endpoint
+    && typeof endpoint === 'object'
+    && Object.keys(endpoint).length === 2
+    && 'protocol' in endpoint
+    && 'name' in endpoint;
+
+  if (isValidEndpoint) {
+    if (!weakMap.has(endpoint)) {
+      weakMap.set(endpoint, 1);
+    } else if (weakMap.get(endpoint) >= 5) {
+      throw new Error('Endpoint load is high');
+    } else {
+      weakMap.set(endpoint, weakMap.get(endpoint) + 1);
+    }
   }
 }
