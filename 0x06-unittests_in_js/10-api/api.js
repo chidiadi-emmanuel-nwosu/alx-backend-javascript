ed579b878dc388500de68868d9370100011abc37
chidiadi-emmanuel-nwosu <chidiadi.emmanuel.nwosu@gmail.com>
2024-02-01 17:29:48 +0100

    added more task

---
+++ 0x06-unittests_in_js/10-api/api.js
@@ -0,0 +1,36 @@
+const express = require('express');
+
+const app = express();
+const port = 7865;
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Welcome to the payment system');
+});
+
+app.get('/cart/:id(\\d+)', (req, res) => {
+  const { id } = req.params;
+  res.send(`Payment methods for cart ${id}`);
+});
+
+app.get('/available_payments', (req, res) => {
+  const response = {
+    payment_methods: {
+      credit_cards: true,
+      paypal: false
+    }
+  };
+  res.send(response);
+});
+
+app.post('/login', (req, res) => {
+  const { userName } = req.body;
+  res.send(`Welcome ${userName}`);
+});
+
+app.listen(port, () => {
+  console.log('API available on localhost port 7865');
+});
+
+module.exports = app;
